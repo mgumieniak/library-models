99048f063961d39628ecb4154bad874a5739d256
mgumieniak <maciejgumieniak007@gmail.com>
2019-12-22 00:21:58 +0100

    Init commit

---
+++ src/main/java/com/database/UserPrincipalDto.java
@@ -0,0 +1,55 @@
+package com.database;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserPrincipalDto implements UserDetails {
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+    private boolean active;
+    private boolean accountExpire;
+    private boolean locked;
+    private boolean credentialsExpire;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return !this.accountExpire;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !this.locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return !this.credentialsExpire;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return !this.active;
+    }
+}
